f50ca142a41641acdb0a2d53c72d31ee9f8e97c7
shjwudp <shjwudp@gmail.com>
2021-07-09 12:51:37 +0800

    feat: add http kv store module

---
+++ bagua-store/src/lib.rs
@@ -0,0 +1,182 @@
+pub mod memdb;
+
+use crate::memdb::Memdb;
+use tonic::{Request, Response, Status};
+
+pub mod bagua {
+    pub mod kv_store {
+        tonic::include_proto!("bagua.kv_store"); // The string specified here must match the proto package name
+    }
+}
+
+use bagua::kv_store::kv_store_server::{KvStore, KvStoreServer};
+use bagua::kv_store::{
+    DeleteRequest, DeleteResponse, GetRequest, GetResponse, SetRequest, SetResponse,
+};
+
+pub type BaguaKvStoreServer<T> = bagua::kv_store::kv_store_server::KvStoreServer<T>;
+
+#[derive(Debug)]
+pub struct KvStoreService {
+    memdb: Memdb,
+}
+
+impl KvStoreService {
+    pub fn new() -> Self {
+        Self {
+            memdb: Memdb::open(),
+        }
+    }
+}
+
+#[tonic::async_trait]
+impl KvStore for KvStoreService {
+    async fn set(&self, request: Request<SetRequest>) -> Result<Response<SetResponse>, Status> {
+        let request = request.into_inner();
+        self.memdb.set(request.key, request.value);
+
+        Ok(Response::new(SetResponse {}))
+    }
+
+    async fn get(&self, request: Request<GetRequest>) -> Result<Response<GetResponse>, Status> {
+        let request = request.into_inner();
+
+        let value = self.memdb.get(request.key);
+        if value.is_none() {
+            return Err(Status::invalid_argument(format!("{:?}", value)));
+        }
+
+        Ok(Response::new(GetResponse {
+            value: value.unwrap(),
+        }))
+    }
+
+    async fn delete(
+        &self,
+        request: Request<DeleteRequest>,
+    ) -> Result<Response<DeleteResponse>, Status> {
+        let request = request.into_inner();
+
+        self.memdb.del(request.key);
+
+        Ok(Response::new(DeleteResponse {}))
+    }
+}
+
+use bagua::kv_store::kv_store_client::KvStoreClient;
+use tokio::runtime::Builder;
+use std::io;
+
+type StdError = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+pub struct BaguaKvStore {
+    client: KvStoreClient<tonic::transport::Channel>,
+    rt: tokio::runtime::Runtime,
+}
+
+impl BaguaKvStore {
+    pub fn open<D>(dst: D) -> Result<Self, io::Error>
+    where
+        D: std::convert::TryInto<tonic::transport::Endpoint>,
+        D::Error: Into<StdError>,
+    {
+        let rt = Builder::new_multi_thread().enable_all().build().unwrap();
+        let client = rt.block_on(KvStoreClient::connect(dst));
+        match client {
+            Ok(client) => Ok(Self { rt, client }),
+            Err(err) => Err(io::Error::new(io::ErrorKind::Other, format!("{:?}", err))),
+        }
+    }
+
+    /// Set a value in the database.
+    pub fn set(&mut self, key: String, value: impl AsRef<[u8]>) -> Result<(), io::Error> {
+        let request = tonic::Request::new(SetRequest {
+            key: key,
+            value: value.as_ref().to_owned(),
+        });
+        let rsp = self.rt.block_on(self.client.set(request));
+        match rsp {
+            Ok(_) => Ok(()),
+            Err(err) => Err(io::Error::new(io::ErrorKind::Other, format!("{:?}", err))),
+        }
+    }
+
+    pub fn get(&mut self, key: String) -> Result<Vec<u8>, io::Error> {
+        let request = tonic::Request::new(GetRequest { key: key });
+        let rsp = self.rt.block_on(self.client.get(request));
+        match rsp {
+            Ok(rsp) => Ok(rsp.into_inner().value),
+            Err(err) => Err(io::Error::new(io::ErrorKind::Other, format!("{:?}", err))),
+        }
+    }
+
+    pub fn del(&mut self, key: String) -> Result<(), io::Error> {
+        let request = tonic::Request::new(DeleteRequest { key: key });
+        let rsp = self.rt.block_on(self.client.delete(request));
+        match rsp {
+            Ok(_) => Ok(()),
+            Err(err) => Err(io::Error::new(io::ErrorKind::Other, format!("{:?}", err))),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    use tokio::runtime::Runtime;
+    use bagua::kv_store::kv_store_server::KvStoreServer;
+
+    #[test]
+    fn test_kvstore_service() {
+        let service = KvStoreService::new();
+
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            let _ = service
+                .set(tonic::Request::new(SetRequest {
+                    key: "key1".into(),
+                    value: "123".as_bytes().to_vec(),
+                }))
+                .await;
+            let rsp = service
+                .get(tonic::Request::new(GetRequest { key: "key1".into() }))
+                .await;
+            assert_eq!(rsp.unwrap().into_inner().value, "123".as_bytes().to_vec());
+            let _ = service
+                .delete(tonic::Request::new(DeleteRequest { key: "key1".into() }))
+                .await;
+            let rsp = service
+                .get(tonic::Request::new(GetRequest { key: "key1".into() }))
+                .await;
+            assert_eq!(rsp.unwrap_err().code(), tonic::Code::InvalidArgument);
+        });
+    }
+
+    // #[test]
+    // fn test_bagua_kv_store() {
+    //     let addr = "[::1]:50051";
+    //     let service_addr = addr.clone().parse().unwrap();
+    //     let kv_store = KvStoreService::new();
+
+    //     let rt = Runtime::new().unwrap();
+    //     let service_fut = tonic::transport::Server::builder()
+    //         .add_service(KvStoreServer::new(kv_store))
+    //         .serve(service_addr);
+    
+    //     std::thread::spawn(move || {
+    //         let one_sec = std::time::Duration::from_secs(1);
+    //         std::thread::sleep(one_sec);
+    
+    //         let mut kv = BaguaKvStore::open("http://[::1]:50051").unwrap();
+    //         kv.set("key1".into(), "123".as_bytes()).unwrap();
+    //         assert_eq!(kv.get("key1".into()).unwrap(), "123".as_bytes().to_vec());
+    //         kv.del("key1".into()).unwrap();
+    //         kv.del("key1".into()).unwrap_err();
+    //         kv.get("key1".into()).unwrap_err();
+    //     });
+    
+    //     rt.block_on(service_fut)
+    //         .expect("failed to successfully run the future on RunTime");
+    // }
+}
---
+++ examples/single_process_multi_gpu.rs
@@ -0,0 +1,169 @@
+use bagua_core_internal::communicators::{BaguaCommOpConfig, BaguaSingleCommunicator};
+use bagua_core_internal::datatypes::{BaguaBucket, BaguaTensor};
+use bagua_core_internal::telemetry::{
+    BaguaCommCoreTelemetry, RegisterModelsRequest, TensorDeclaration,
+};
+use bagua_store::{BaguaKvStore, BaguaKvStoreServer, KvStoreService};
+use nix::{
+    sys::wait::waitpid,
+    unistd::{fork, ForkResult},
+};
+use std::{
+    process::{exit, Command},
+    thread::sleep,
+    thread,
+    time,
+    time::Duration,
+};
+use tokio::runtime::{Builder, Runtime};
+use tonic::{Request, Response, Status};
+
+fn init_process_group(gpu_setting: Vec<i32>, nranks: i32, master_addr: String, master_port: i32) {
+    let mut kv = loop {
+        let kv = BaguaKvStore::open(format!("http://{}:{}", master_addr, master_port));
+        match kv {
+            Err(err) => {
+                println!("BaguaKvStore::open failed, err={:?}", err);
+                thread::sleep(time::Duration::from_secs(1));
+            },
+            Ok(kv) => break kv,
+        }
+    };
+
+    let nccl_unique_id = if gpu_setting.iter().any(|&i| i == 0) {
+        let nccl_unique_id = BaguaSingleCommunicator::generate_nccl_unique_id_str().as_bytes();
+        kv.set("nccl_unique_id".into(), nccl_unique_id).unwrap();
+
+        nccl_unique_id
+    } else {
+        let nccl_unique_id = loop {
+            let nccl_unique_id = kv.get("nccl_unique_id".into());
+            match nccl_unique_id {
+                Err(err) => {
+                    println!("kv.get nccl_unique_id failed, err={:?}", err);
+                    thread::sleep(time::Duration::from_secs(1));
+                },
+                Ok(nccl_unique_id) => break nccl_unique_id,
+            }
+        };
+        println!("nccl_unique_id={:?}", nccl_unique_id);
+        assert_eq!(nccl_unique_id, "123".as_bytes());
+
+        nccl_unique_id
+    };
+
+    let comm_init_threads = Vec::new();
+    for gpu_id in gpu_setting {
+        let t = std::thread::spawn(move || {
+            BaguaSingleCommunicator::new(
+                rank,
+                nranks,
+                device_id,
+                stream_ptr,
+                std::str::from_utf8(nccl_unique_id).unwrap(),
+            )
+        });
+        comm_init_threads.push(t);
+    }
+
+    let comm_list = Vec::new();
+    for t in comm_init_threads {
+        comm_list.push(t.join());
+    }
+
+    for communicator in comm_list {
+        println!("rank={} ready!", communicator.rank());
+    }
+}
+
+fn main() {
+    let nranks = 8;
+    let master_addr = "127.0.0.1";
+    let master_port = 8123;
+
+    let mut child_id_list = Vec::new();
+    let processes_gpu_setting = vec![vec![0], vec![1, 2], vec![3, 4, 5, 6, 7]];
+    for gpu_setting in processes_gpu_setting {
+        match fork().expect("Failed to fork process") {
+            ForkResult::Parent { child } => {
+                // println!("Try to kill me to check if the target process will be killed");
+                child_id_list.push(child);
+                // // Do not forget to wait for the fork in order to prevent it from becoming a zombie!!!
+                // waitpid(Some(child), None).unwrap();
+                // // You have 120 seconds to kill the process :)
+                // sleep(Duration::from_secs(120));
+            }
+            ForkResult::Child => {
+                println!("gpu_setting={:?}", gpu_setting);
+                // let (sender, receiver) = std::sync::mpsc::channel();
+                let kv_store = if gpu_setting.iter().any(|&i| i == 0) {
+                    Some(std::thread::spawn(move || {
+                        // match sender.send(std::net::TcpStream::connect(("127.0.0.1", 12333))) {
+                        //     Ok(()) => {
+                        //         let rt = Runtime::new().unwrap();
+                        //         let kv_store = KvStoreService::new();
+                        //         let service_addr = format!("{}:{}", master_addr, master_port);
+                        //         println!("{} listen on service_addr={:?}", std::process::id(), service_addr);
+                        //         let service_fut = tonic::transport::Server::builder()
+                        //             .add_service(BaguaKvStoreServer::new(kv_store))
+                        //             .serve(service_addr.parse().unwrap());
+                        //         rt.block_on(service_fut)
+                        //             .expect("failed to successfully run the future on RunTime");
+                        //     }, // everything good
+                        //     Err(_) => {}, // we have been released, don't panic
+                        // }
+                        let rt = Runtime::new().unwrap();
+                        let kv_store = KvStoreService::new();
+                        let service_addr = format!("{}:{}", master_addr, master_port);
+                        println!("{} listen on service_addr={:?}", std::process::id(), service_addr);
+                        let service_fut = tonic::transport::Server::builder()
+                            .add_service(BaguaKvStoreServer::new(kv_store))
+                            .serve(service_addr.parse().unwrap());
+                        rt.block_on(service_fut)
+                            .expect("failed to successfully run the future on RunTime");
+                    }))
+                } else {
+                    None
+                };
+
+                // let (sender, receiver) = mpsc::channel();
+                // let t = thread::spawn(move || {
+                //     match sender.send(std::net::TcpStream::connect(("127.0.0.1", 12333))) {
+                //         Ok(()) => {
+                //             let rt = Runtime::new().unwrap();
+                //             let kv_store = KvStoreService::new();
+                //             let service_addr = format!("{}:{}", master_addr, master_port);
+                //             println!("{} listen on service_addr={:?}", std::process::id(), service_addr);
+                //             let service_fut = tonic::transport::Server::builder()
+                //                 .add_service(BaguaKvStoreServer::new(kv_store))
+                //                 .serve(service_addr.parse().unwrap());
+                //             rt.block_on(service_fut)
+                //                 .expect("failed to successfully run the future on RunTime");
+                //         }, // everything good
+                //         Err(_) => {}, // we have been released, don't panic
+                //     }
+                // });
+                // return receiver.recv_timeout(Duration::from_millis(5000));
+
+                init_process_group(gpu_setting, nranks, master_addr.into(), master_port);
+
+                if let Some(server_thread) = kv_store {
+                    thread::sleep(time::Duration::from_secs(5));
+                    // server_thread.join();
+                }
+                exit(0);
+            }
+        }
+    }
+
+    for child_id in child_id_list {
+        waitpid(child_id, None).unwrap();
+    }
+
+    // // 1, 2, 5
+    // if let Ok(Fork::Child) = daemon(false, false) {
+    //     println!("aabb");
+    // }
+
+    println!("ccdd");
+}
