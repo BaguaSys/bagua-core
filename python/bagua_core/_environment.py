494c88895728b802478ced08133e7905902d916a
shjwudp <shjwudp@gmail.com>
2021-09-07 15:30:00 +0800

    feat: optional load bagua-net

--- python/bagua_core/_environment.py
+++ python/bagua_core/_environment.py
@@ -6,3 +6,8 @@ def _preload_libraries():
     cwd = os.path.dirname(os.path.abspath(__file__))
     libnccl_path = os.path.join(cwd, ".data", "lib", "libnccl.so")
     ctypes.CDLL(libnccl_path)
+
+    # Load bagua-net
+    if os.environ.get("ENABLE_BAGUA_NET", "0") == "1":
+        ctypes.CDLL(os.path.join(cwd, ".data", "lib", "libbagua_net.so"))
+        ctypes.CDLL(os.path.join(cwd, ".data", "lib", "libnccl-net.so"))
